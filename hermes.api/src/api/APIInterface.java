52a92d787e4164f588fa88783f1ea38cf9e3d495
henribouvet <bouvet.henri@gmail.com>
2019-01-30 20:26:08 +0100

    Micro interface

---
+++ hermes.api/src/api/APIInterface.java
@@ -0,0 +1,49 @@
+package api;
+
+public class APIInterface {
+
+    public boolean store(Object data) {
+
+        //if tomp2p
+
+        //if kad
+
+        return true;
+    }
+
+    public Object retrieve(Object key) {
+
+        //if tomp2p
+
+        //if kad
+
+        return "Data";
+    }
+
+    public boolean put(Object data) {
+
+        //if tomp2p
+
+        //if kad
+
+        return true;
+    }
+
+    public Object get(Object key) {
+
+        //if tomp2p
+
+        //if kad
+
+        return "Data";
+    }
+
+    public boolean remove(Object data) {
+
+        //if tomp2p
+
+        //if kad
+
+        return true;
+    }
+}
