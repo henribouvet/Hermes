cfa769b474d7760be35cc03d8eb7e96c3ce1b191
henribouvet <bouvet.henri@gmail.com>
2019-01-30 18:41:00 +0100

    First commit

---
+++ hermes.api/src/api/App.java
@@ -0,0 +1,13 @@
+package api;
+
+import net.tomp2p.dht.FutureDHT;
+import net.tomp2p.p2p.Peer;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World !");
+        Peer peer;
+        FutureDHT dht;
+    }
+}
