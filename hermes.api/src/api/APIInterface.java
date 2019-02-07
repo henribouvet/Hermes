bb95997dabc482203cf5c71e166400eab56173bd
henribouvet <bouvet.henri@gmail.com>
2019-02-07 20:29:31 +0100

    Really small injection

--- hermes.api/src/api/APIInterface.java
+++ hermes.api/src/api/APIInterface.java
@@ -2,41 +2,17 @@ package api;
 
 import adapters.Adapters;
 
+import javax.inject.Inject;
+import java.io.Serializable;
 import java.util.concurrent.Future;
 
-public class APIInterface {
+public class APIInterface<K extends Serializable, V extends Serializable> {
 
+    @Inject
     private Adapters adapter;
 
 
-    public APIInterface(Adapters adapter) {
-        this.adapter = adapter;
-    }
-
-    public boolean store(Object data) {
-
-        return adapter.store();
-    }
-
-    public Future<Object> retrieve(Object key) {
-
-        return adapter.retrieve(key);
-    }
-
-    public boolean put(Object data) {
-
-        return adapter.put(data);
-    }
-
-    public Object get(Object key) {
 
 
-        return adapter.get(key);
-    }
 
-    public boolean remove(Object data) {
-
-
-        return adapter.remove(data);
-    }
 }
